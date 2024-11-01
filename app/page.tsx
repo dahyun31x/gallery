dfcbadd5e273bc1b969140156415f9124347c58c
dahyun31x <dahyun31x@gmail.com>
2024-11-01 16:19:18 +0900

    레이아웃 수정, 컴포넌트 제거

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,36 @@
-import React from 'react';
-import Profile from './components/profilfe';
- 
-export default function Home() {
+"use client";
+
+import React, { useState } from "react";
+import styles from "./page.module.scss";
+import Content from "./components/Content";
+
+export default function Page() {
+  const tabData = [
+    { btn: "일상 🏃‍♀️", content: "example 2" },
+    { btn: "독후감 📚", content: "example 1" },
+    { btn: "홈 🏡", content: <><h1>블로그 홈</h1><br/><p>어서오세요</p></> },
+  ];
+
+  const [activeTab, setActiveTab] = useState(2);
+
   return (
-    <>
-      <Profile/>  
-    </>
+    <div className={styles.root}>
+      <div className={styles.tabListContentWrapper}>
+        <ul className={styles.tabList}>
+          {tabData.map((tab, index) => (
+            <li
+              key={index}
+              className={index === activeTab ? styles.activeTab : ""}
+              onClick={() => setActiveTab(index)}
+            >
+              {tab.btn}
+            </li>
+          ))}
+        </ul>
+
+        <div className={styles.content}>{tabData[activeTab].content}</div>
+        <div></div>
+      </div>
+    </div>
   );
 }
